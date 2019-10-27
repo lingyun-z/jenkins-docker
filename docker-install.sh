04edfeb212d1421973a6c7a4bf1dc39d296cf30f
nico <474630752@qq.com>
2019-10-27 20:38:12 +0800

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,7 +1,6 @@
 sudo apt-get update
 curl -fsSL get.docker.com -o get.docker.sh
 sh get.docker.sh --mirror Aliyun
-sudo echo {"registry-mirror": ["https://registry.docker-cn.com"]} > /etc/docker/daemon.json
 sudo systemctl restart docker
 sudo apt-get install docker-compose
 rm get.docker.sh
